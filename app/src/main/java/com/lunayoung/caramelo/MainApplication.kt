fea3abaf5503067540e720b886d8e3fddb7c0ce6
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2019-09-22 19:55:07 +0900

    애플리케이션 클래스 생성 및 매니페스트에 추가

---
+++ app/src/main/java/com/lunayoung/caramelo/MainApplication.kt
@@ -0,0 +1,40 @@
+package com.lunayoung.caramelo
+
+import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+
+
+class MainApplication : Application(){
+
+    private val PRINT_STACK_COUNT : Int =  5
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MainApplication? = null
+
+        fun applicationContext() : MainApplication {
+            return instance as MainApplication
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Logger Stack trace을 PRINT_STACK_COUNT 개로 조절
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+            .methodCount(PRINT_STACK_COUNT)
+            .build()
+
+        // Release 빌드 시 로그 제거
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
+    }
+}
